656baaac9f3d2923b5aea4cb09b68188dd7a2140
Elmer <contact@elmer.dev>
2023-01-21 15:53:50 -0800

    text to asl and webscraper

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,56 @@
 const dotenv = require('dotenv');
-const app = require('express');
+const express = require('express');
+const cors = require('cors');
+const got = require('got');
+const cheerio = require('cheerio');
 
 dotenv.config();
+
+// Express App
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.get('/api/webscrape/:word', (req, res) => {
+  const { word } = req.params
+
+  got(`https://www.signingsavvy.com/search/${word}`).then(result => {
+    let $ = cheerio.load(result.body);
+    let video = $("#video-1 source").attr('src');
+
+    if (!video || video === null) {
+      const search_result = $(".search_results ul li:first-child a").attr('href');
+
+      got(`https://www.signingsavvy.com/${search_result}`).then(result => {
+        let $ = cheerio.load(result.body);
+        let video = $("#video-1 source").attr('src');
+
+        res.json({ 'video_url': video })
+      }).catch(err => {
+        console.log(err);
+      });
+    } else {
+      res.json({ 'video_url': video })
+    }
+
+    
+  }).catch(err => {
+      console.log(err);
+  });
+
+})
+
+app.get('/api/webscrape/', (req, res) => {
+  res.json({ 'video_url': 'No word input' })
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send('OK');
+});
+
+// Listen
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
