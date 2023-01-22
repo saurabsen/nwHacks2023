96348c47ef4bec23ec97e1912c7e070de7f51783
Elmer <contact@elmer.dev>
2023-01-22 07:47:06 -0800

    added translation to capture screen

--- frontend/src/view/Translate.jsx
+++ frontend/src/view/Translate.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, TextareaAutosize, Select, MenuItem, Typography, FormControl, InputLabel } from '@mui/material'
 import { LanguageSelect } from '../components/LanguageSelect'
@@ -6,6 +6,9 @@ import { Box } from '@mui/system'
 import './Capture.css'
 
 export const Translate = () => {
+  const [sourceText, setSourceText] = useState("")
+
+  
 
   const navigator = useNavigate();
 
@@ -19,7 +22,7 @@ export const Translate = () => {
       <Box style={{paddingLeft: '1rem', paddingRight: '1rem', display: 'flex', flexDirection: 'column', gap: '1rem'}}>
         <Typography>Translate from</Typography>
         <FormControl style={{width: '100%'}}>
-          <LanguageSelect idName="Capture" />
+          <LanguageSelect idName="Capture" sourceText={sourceText} targetLang="EN"  />
         </FormControl>
         <TextareaAutosize minRows={3}></TextareaAutosize>
         <Button variant="contained">Translate to ASL</Button>
